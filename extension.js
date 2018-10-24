d8409ca7cf972b1400b6137726b7d411060c6855
Thorsten Lorenz <thlorenz@gmx.de>
2018-10-24 10:18:31 -0500

    status: showing deopts count in status bar
    - activating summary view via click on warning in status

--- extension.js
+++ extension.js
@@ -1,9 +1,12 @@
 'use strict'
 
 const path = require('path')
+const { commands } = require('vscode')
+
 const DeoptigateWatcher = require('./lib/deoptigate-watcher')
 const DeoptigateProcessor = require('./lib/deoptigate-processor')
 const DeoptigateSummaryView = require('./lib/deoptigate-summary-view')
+const DeoptigateStatusbar = require('./lib/deoptigate-statusbar')
 
 function activate(context) {
   // TODO: figure out where we want the log files and how to make the
@@ -14,10 +17,16 @@ function activate(context) {
   const watcher = new DeoptigateWatcher(root)
   const deoptigateProcessor = new DeoptigateProcessor(projectRoot)
   const summaryView = new DeoptigateSummaryView()
+  const statusbar = new DeoptigateStatusbar()
+
+  const showSummaryCommand =
+    commands.registerCommand('deoptigate:toggle-summary', () => summaryView.toggle())
+  context.subscriptions.push(showSummaryCommand)
 
   function ondeoptigateUpdate(info) {
     const res = deoptigateProcessor.process(info)
     summaryView.update(res)
+    statusbar.update(res)
   }
 
   watcher
